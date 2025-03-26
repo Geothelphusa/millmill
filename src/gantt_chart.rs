0de205a5ba6fb67433b6f8ad217069d7b761ee26
YoshitakaNaraoka <mikoprpr@hotmail.com>
2025-03-27 02:43:51 +0900

    pre-commit

--- src/gantt_chart.rs
+++ src/gantt_chart.rs
@@ -1,150 +1,155 @@
-use std::cell::RefCell;
 use std::rc::Rc;
-
-use chrono::Datelike;
+use chrono::{Duration, NaiveDateTime};
 use stylist::yew::styled_component;
 use wasm_bindgen::JsCast;
+
+use crate::styles::*;
 use yew::prelude::*;
-use crate::styles::*; // スタイルモジュールをインポート
 
-use chrono::{NaiveDateTime, Duration};
-use gloo_events::EventListener;
-
-#[derive(Clone, PartialEq)]
+#[derive(Clone, PartialEq, Debug)]
 struct Task {
     id: usize,
-    name: &'static str,
+    name: String,
     start_date: NaiveDateTime,
     end_date: NaiveDateTime,
-    color: &'static str,
+    color: String,
 }
 
 fn initial_tasks() -> Vec<Task> {
     let base_date = NaiveDateTime::parse_from_str("2025-03-01 00:00:00", "%Y-%m-%d %H:%M:%S").unwrap();
     vec![
-        Task { id: 1, name: "Task 1", start_date: base_date, end_date: base_date + Duration::days(5), color: "#4CAF50" },
-        Task { id: 2, name: "Task 2", start_date: base_date + Duration::days(6), end_date: base_date + Duration::days(9), color: "#FF9800" },
-        Task { id: 3, name: "Task 3", start_date: base_date + Duration::days(10), end_date: base_date + Duration::days(14), color: "#673AB7" },
+        Task { id: 1, name: "Task 1".to_string(), start_date: base_date, end_date: base_date + Duration::days(5), color: "#4CAF50".to_string() },
+        Task { id: 2, name: "Task 2".to_string(), start_date: base_date + Duration::days(6), end_date: base_date + Duration::days(9), color: "#FF9800".to_string() },
+        Task { id: 3, name: "Task 3".to_string(), start_date: base_date + Duration::days(10), end_date: base_date + Duration::days(14), color: "#673AB7".to_string() },
     ]
 }
 
 #[styled_component(GanttChart)]
 pub fn gantt_chart() -> Html {
-
     let tasks = use_state(initial_tasks);
-    let from_date_ref = use_state(|| Rc::new(RefCell::new(None)));
-    let dragging_task_id = use_state(|| Rc::new(RefCell::new(None)));
+    let zoom_level = use_state(|| 50);
+    let scroll_offset = use_state(|| 0);
+    let selected_task = use_state(|| None::<Rc<Task>>);
 
-    let on_mouse_down = {
-        let from_date_ref = from_date_ref.clone();
-        let dragging_task_id = dragging_task_id.clone();
-        Callback::from(move |(e, task): (MouseEvent, Task)| {
-            e.prevent_default();
-            from_date_ref.set(Rc::new(RefCell::new(Some(task.start_date))));
-            dragging_task_id.set(Rc::new(RefCell::new(Some(task.id))));
-        })
-    };
 
-    let on_mouse_move = {
-        let from_date_ref = from_date_ref.clone();
-        let dragging_task_id = dragging_task_id.clone();
+    let add_task = {
         let tasks = tasks.clone();
-        Callback::from(move |e: MouseEvent| {
-            let from_date_rc = &**from_date_ref;
-            let dragging_id_rc = &**dragging_task_id;
-
-            let from_date_option = from_date_rc.borrow();
-            let dragging_id_option = dragging_id_rc.borrow();
-
-            if let Some(_from_date) = *from_date_option {
-                if let Some(dragging_id) = *dragging_id_option {
-                    let diff_days = (e.movement_x() as i64) / 50;
-                    let current_tasks = (*tasks).clone();
-                    let new_tasks: Vec<Task> = current_tasks
-                        .iter()
-                        .map(|task| {
-                            if task.id == dragging_id {
-                                let new_start_date = task.start_date + Duration::days(diff_days);
-                                let new_end_date = task.end_date + Duration::days(diff_days);
-                                Task {
-                                    start_date: new_start_date,
-                                    end_date: new_end_date,
-                                    ..task.clone()
-                                }
-                            } else {
-                                task.clone()
-                            }
-                        })
-                        .collect();
-                    tasks.set(new_tasks);
-                }
-            }
-        })
-    };
-
-    let on_mouse_up = {
-        let from_date_ref = from_date_ref.clone();
-        let dragging_task_id = dragging_task_id.clone();
         Callback::from(move |_| {
-            from_date_ref.set(Rc::new(RefCell::new(None)));
-            dragging_task_id.set(Rc::new(RefCell::new(None)));
+            let mut new_tasks = (*tasks).clone();
+            let id = new_tasks.len() + 1;
+            let base_date = NaiveDateTime::parse_from_str("2025-03-01 00:00:00", "%Y-%m-%d %H:%M:%S").unwrap();
+            new_tasks.push(Task {
+                id,
+                name: format!("Task {}", id),
+                start_date: base_date + Duration::days(id as i64 * 3),
+                end_date: base_date + Duration::days(id as i64 * 5),
+                color: "#009688".to_string(),
+            });
+            tasks.set(new_tasks);
         })
     };
 
-    {
-        let on_mouse_move = on_mouse_move.clone();
-        let on_mouse_up = on_mouse_up.clone();
-        use_effect_with((), move |_| {
-            let move_listener = EventListener::new(&gloo::utils::window(), "mousemove", move |e: &web_sys::Event| {
-                if let Ok(mouse_event) = e.clone().dyn_into::<web_sys::MouseEvent>() {
-                    on_mouse_move.emit(mouse_event);
-                }
-            });
-            let up_listener = EventListener::new(&gloo::utils::window(), "mouseup", move |e: &web_sys::Event| {
-                if let Ok(mouse_event) = e.clone().dyn_into::<web_sys::MouseEvent>() {
-                    on_mouse_up.emit(mouse_event);
-                }
-            });
+    let remove_task = {
+        let tasks = tasks.clone();
+        Callback::from(move |id: usize| {
+            let new_tasks = (*tasks).clone().into_iter().filter(|task| task.id != id).collect();
+            tasks.set(new_tasks);
+        })
+    };
 
-            move || drop((move_listener, up_listener))
-        });
-    }
+    let edit_task = {
+        let tasks = tasks.clone();
+        let selected_task = selected_task.clone();
+        Callback::from(move |(id, name, start, end): (usize, String, NaiveDateTime, NaiveDateTime)| {
+            let new_tasks = (*tasks).clone().into_iter().map(|mut task| {
+                if task.id == id {
+                    task.name = name.clone();
+                    task.start_date = start;
+                    task.end_date = end;
+                }
+                task
+            }).collect();
+            tasks.set(new_tasks);
+            selected_task.set(None);
+        })
+    };
 
-    let tasks_clone = (*tasks).clone();
+    let zoom_in = {
+        let zoom_level = zoom_level.clone();
+        Callback::from(move |_| zoom_level.set((*zoom_level - 10).max(20)))
+    };
+
+    let zoom_out = {
+        let zoom_level = zoom_level.clone();
+        Callback::from(move |_| zoom_level.set((*zoom_level + 10).min(100)))
+    };
+
+    let scroll_left = {
+        let scroll_offset = scroll_offset.clone();
+        Callback::from(move |_| scroll_offset.set((*scroll_offset - 50).max(0)))
+    };
+
+    let scroll_right = {
+        let scroll_offset = scroll_offset.clone();
+        Callback::from(move |_| scroll_offset.set(*scroll_offset + 50))
+    };
+
+    let on_input_name = {
+        let edit_task = edit_task.clone();
+        Callback::from(move |(id, name, start, end): (usize, String, NaiveDateTime, NaiveDateTime)| {
+            edit_task.emit((id, name, start, end));
+        })
+    };
 
     html! {
         <>
-            <div>
-                <h2>{ "Gantt Chart" }</h2>
-                <div class={classes!(grid_style())}>
-                    { for (0..30).map(|_row| html! {
-                        { for (0..30).map(|_col| html! {
-                            <div class={classes!(cell_style())}></div>
-                        }) }
+            <button onclick={add_task}>{ "Add Task" }</button>
+            <button onclick={Callback::from(move |_| {
+                log::info!("tasks: {:?}", *tasks);
+                log::info!("zoom_level: {:?}", *zoom_level);
+            })}>{ "Debug" }</button>
+            <button onclick={zoom_in}>{ "Zoom In" }</button>
+            <button onclick={zoom_out}>{ "Zoom Out" }</button>
+            <button onclick={scroll_left}>{ "Scroll Left" }</button>
+            <button onclick={scroll_right}>{ "Scroll Right" }</button>
+            <div class={classes!("gantt-container")} style={format!("width: {}%;", *zoom_level)}>
+                <div class={classes!(grid_style())} style={format!("transform: translateX(-{}px);", *scroll_offset)}>
+                { for tasks.clone().iter().map(|task| {
+                        let remove_task = remove_task.clone();
+                        let on_input_name = on_input_name.clone();
+                        html! {
+                            <TaskView task={task} remove_task={remove_task} on_input_name={on_input_name} />
+                        }
                     }) }
-                    {
-                        tasks_clone.iter().enumerate().map(|(i, task)| {
-                            let task_clone = task.clone();
-                            let row_index = i + 1;
-                            let start_day = task.start_date.day();
-                            let end_day = task.end_date.day();
-                            let column_start = if start_day > 0 && start_day <= 30 { start_day } else { 1 };
-                            let column_end = if end_day > 0 && end_day <= 30 { end_day + 1 } else { 31 };
-                            html! {
-                                <div
-                                    class={classes!(task_style())}
-                                    style={format!("grid-column-start: {}; grid-column-end: {}; grid-row-start: {}; background: {};", column_start, column_end, row_index, task.color)}
-                                    onmousedown={on_mouse_down.reform(move |e: MouseEvent| (e, task_clone.clone()))}
-                                    onmousemove={on_mouse_move.clone()}
-                                    onmouseup={on_mouse_up.clone()}
-                                >
-                                    { task.name }
-                                </div>
-                            }
-                        }).collect::<Html>()
-                    }
                 </div>
             </div>
         </>
+    }
+}
+
+#[derive(Properties, PartialEq)]
+struct TaskViewProps {
+    task: Task,
+    remove_task: Callback<usize>,
+    on_input_name: Callback<(usize, String, NaiveDateTime, NaiveDateTime)>,
+}
+
+#[function_component(TaskView)]
+fn task_view(props: &TaskViewProps) -> Html {
+    let task = &props.task;
+    let remove_task = props.remove_task.clone();
+    let on_input_name = props.on_input_name.clone();
+    let task = Rc::new(props.task.clone());
+    let task_id = task.id;
+    html! {
+        <div class={classes!(task_style())} style={format!("background: {};", task.color)}>
+            <input
+                value={task.name.clone()}
+                oninput={Callback::from(move |e: InputEvent| {
+                    let input = e.target().unwrap().unchecked_into::<web_sys::HtmlInputElement>();
+                    on_input_name.emit((task.id, input.value(), task.start_date, task.end_date));
+                })} />
+                <button onclick={remove_task.reform(move |_| task_id)}>{ "Delete" }</button>
+        </div>
     }
 }
